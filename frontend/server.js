791ce2127a282f257ef30c45bc050a7178e5a7a0
Saulo Felipe <saulofelipe234567@gmail.com>
2021-07-06 22:44:31 -0300

    fix deploy 0.3

--- frontend/server.js
+++ frontend/server.js
@@ -2,17 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-
 app.use(express.static(path.join(__dirname, 'build')))
 
-
-app.get('/*', (request, response) => {
+app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'build', 'index.html'))
-});
+})
 
-
-app.listen(process.env.PORT || 3000, (err) => {
-	if (err) return console.log('Erro Encontrado: '+err)
+app.listen(process.env.PORT || 3000, (error) => {
+	if (error) return console.log(`==========| Erro encontrado: |==========\n${error}`)
 
 	console.log('Server is Running')
 })
